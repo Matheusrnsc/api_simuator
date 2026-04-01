cdd37ea99c75e9d915ba725e51daf58119e75aa3
Matheus Nascimento <matheusjjy@gmail.com>
2026-03-31 23:28:04 -0300

    feat: inicializa projeto Spring Boot e endpoint /ping

---
+++ simulador/src/test/java/com/api_emprestimo/simulador/SimuladorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_emprestimo.simulador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
